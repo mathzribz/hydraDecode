eecdb646f186a727bc5d053534e4156f1042d3db
mathe <matheusbacellarribeiro@gmail.com>
2025-11-24 09:00:45 -0300

    é amigos

--- TeamCode/src/main/java/all/tests/testesshootpidf.java
+++ TeamCode/src/main/java/all/tests/testesshootpidf.java
@@ -1,4 +1,3 @@
-
 package all.tests;
 
 import com.acmerobotics.dashboard.FtcDashboard;
@@ -8,103 +7,103 @@ import com.arcrobotics.ftclib.controller.PIDController;
 import com.arcrobotics.ftclib.controller.wpilibcontroller.SimpleMotorFeedforward;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
-import dev.nextftc.ftc.ActiveOpMode;
-
-@Config
 @TeleOp
+@Config
 public class testesshootpidf extends LinearOpMode {
 
-    VoltageSensor vs;
-    private DcMotorEx AR, AL, KITL, KITR;
+    private DcMotorEx AR, AL;
+    private VoltageSensor battery;
 
-    public static double kP = 0.8;
-    public static double kD = 0.0;
-    public static double kI = 5;
-    public static double kV = 10.0;
-    public static double kA = 10.0;
-    public static double kS = 10.0;
-    public static double AA = 5;
-    public static double VV = 5;
+    // PID constants
+    public static double kP = 0.0040;
+    public static double kI = 0.0;
+    public static double kD = 0.00010;
 
-    public static double speed = 10
-            ;
+    // Feedforward constants
+    public static double kS = 0.15;
+    public static double kV = 0.00125;
+    public static double kA = 0.00008;
 
-        SimpleMotorFeedforward feedforward =
-                new SimpleMotorFeedforward(kS, kV, kA);
+    public static double targetRPM = 3000;
 
-    PIDController pid = new PIDController(kP, kI, kD);
-
-
-        double ff = feedforward.calculate(VV,AA) ;
-
-
-
-
-
-
+    private PIDController pid = new PIDController(kP, kI, kD);
+    private SimpleMotorFeedforward ff = new SimpleMotorFeedforward(kS, kV, kA);
 
     @Override
     public void runOpMode() throws InterruptedException {
 
+        AR = hardwareMap.get(DcMotorEx.class, "shooterR");
+        AL = hardwareMap.get(DcMotorEx.class, "shooterL");
+        battery = hardwareMap.voltageSensor.iterator().next();
 
-        pid.setPID(kP,kI,kD);
+        AR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        AL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        AR.setDirection(DcMotor.Direction.FORWARD);
+        AL.setDirection(DcMotor.Direction.REVERSE);
 
-
-
-
-        telemetry = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry());
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
         waitForStart();
-        while (opModeIsActive()) {
 
-            AR = hardwareMap.get(DcMotorEx.class, "shooterR"); // porta 0
-            AL = hardwareMap.get(DcMotorEx.class, "shooterL"); // porta 0
-            vs = hardwareMap.voltageSensor.iterator().next();
-
-            double voltage = vs.getVoltage();
-
-            AR.setDirection(DcMotor.Direction.REVERSE);
-            AL.setDirection(DcMotor.Direction.REVERSE);
-
-            double result = pid.calculate(AR.getCurrentPosition(), AL.getCurrentPosition() ) + ff;
+        while(opModeIsActive()) {
 
 
-     if (gamepad1.right_trigger > 0.1){
-         AR.setVelocity(result * speed );
-         AL.setVelocity(result * speed );
+            double vr = AR.getVelocity();
+            double vl = AL.getVelocity();
+            double vAvg = (vr + vl) / 2.0;
+
+            double pidPower = pid.calculate(vAvg, targetRPM);
 
 
-     }
-     else {
-         AR.setPower(0);
-         AL.setPower(0);
+            double ffPower = ff.calculate(targetRPM);
 
-     }
+            // ajustar feedforward com compensação de voltagem
+            double voltage = battery.getVoltage();
+            double compensatedFF = ffPower * (12.0 / Math.max(10.0, voltage));
 
-        if (voltage > 12.5 ){
-            speed = 10;
-        }
+            double finalPower = pidPower + compensatedFF;
 
 
-            telemetry.addData("shooterR", AR.getPower());
-            telemetry.addData("shooterL", AL.getPower());
-            telemetry.addData("VL ", AL.getVelocity());
-            telemetry.addData("VR ", AR.getVelocity());
+            if (gamepad1.right_trigger > 0.1) {
+                AR.setPower(finalPower);
+                AL.setPower(finalPower);
+            } else {
+                AR.setPower(0);
+                AL.setPower(0);
+                pid.reset();
+            }
 
-            telemetry.addData("voltage ", voltage);
-            telemetry.addData("speed ", speed);
 
 
+            if (gamepad1.a) {
+                targetRPM = 2000;
+            }
+            if (gamepad1.b) {
+                targetRPM = 1000;
+
+            } if (gamepad1.x) {
+                targetRPM = 500;
+            }
+
+
+
+
+            telemetry.addData("RPM avg", vAvg);
+            telemetry.addData("targetRPM", targetRPM);
+            telemetry.addData("PID", pidPower);
+            telemetry.addData("FF", ffPower);
+            telemetry.addData("FF compensado", compensatedFF);
+            telemetry.addData("Voltagem", voltage);
+            telemetry.addData("FinalPower", finalPower);
+            telemetry.addData("RPM R", vr);
+            telemetry.addData("RPM L", vl);
             telemetry.update();
         }
-
     }
-
 }
