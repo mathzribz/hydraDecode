6f44f927adbc841083832a85ba91cfb6e0fdf08a
Dell <kathlenbomfimenor@gmail.com>
2026-01-13 09:38:07 -0300

    3

---
+++ TeamCode/src/main/java/all/Commands/Shooter/ShooterOff.java
@@ -0,0 +1,30 @@
+
+package all.Commands.Shooter;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import all.subsystems.Intake;
+import all.subsystems.Shooter;
+
+public class ShooterOff extends CommandBase {
+
+    private final Shooter shoot;
+
+    public ShooterOff(Shooter subsystem) {
+        shoot = subsystem;
+        addRequirements(shoot);
+    }
+
+    @Override
+    public void initialize() {
+        shoot.ShooterOff();
+
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+
+}
